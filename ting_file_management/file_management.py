b1167f4efd21c95c479eea0f334946e7418ee45a
Eder Santos <3derms@gmail.com>
2022-04-05 20:27:01 -0300

    feat: cria funçao para importar noticias com sucesso

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,8 @@
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    data_list = []
+    with open(path_file) as file:
+        for row in file:
+            new_row = row[:-1]
+            if len(row) > 2:
+                data_list.append(new_row)
+    return data_list
