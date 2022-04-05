8b927a610a0fc45b8651b0f11782ea06a2d5e0a5
Eder Santos <3derms@gmail.com>
2022-04-05 16:45:11 -0300

    feat: cria o metodo enqueue para adicionar um valor na fila

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,12 +1,14 @@
 class Queue:
+    FIRST_ELEMENT = 0
+
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self._data = []
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self._data)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self._data.append(value)
 
     def dequeue(self):
         """Aqui irá sua implementação"""
