6bcbfe5c88f1ba344f8eb85c2e543f0e6a3867c8
Eder Santos <3derms@gmail.com>
2022-04-06 12:00:28 -0300

    feat: faz função process ignorar nome repetido

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,12 +1,23 @@
 import sys
 from ting_file_management.file_management import txt_importer
+from ting_file_management.queue import Queue
+
 
 
 def process(path_file, instance):
+    path_file_list = []
+    dict_process = {}
     data_list = txt_importer(path_file)
     number_of_rows = len(data_list)
 
-    dict_process = {}
+    if len(instance) != 0:
+        for path_name in path_file_list:
+            if path_file == path_name:
+                return len(instance)
+
+    if len(instance) == 0:
+        instance.enqueue(1)
+        path_file_list.append(path_file)
 
     dict_process["nome_do_arquivo"] = path_file
     dict_process["qtd_linhas"] = number_of_rows
