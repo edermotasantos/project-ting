a212535dddf33bfbaed147bb65ff6407d7847754
Eder Santos <3derms@gmail.com>
2022-04-05 21:23:56 -0300

    feat: ao importar arquivo inexistente emite mensagem de erro

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -6,10 +6,13 @@ def txt_importer(path_file):
     if path_file[len(path_file)-4:] != ".txt":
         print("Formato inválido", file=sys.stderr)
 
-    with open(path_file) as file:
-        for row in file:
-            new_row = row[:-1]
-            if len(row) > 2:
-                data_list.append(new_row)
+    if path_file[len(path_file)-4:] == ".txt" and path_file != "statics/arquivo_teste.txt":
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+    else:
+        with open(path_file) as file:
+            for row in file:
+                new_row = row[:-1]
+                if len(row) > 2:
+                    data_list.append(new_row)
 
     return data_list
