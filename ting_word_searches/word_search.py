6a0f40ea04638ffd685030aa505c1966d8156853
Eder Santos <3derms@gmail.com>
2022-04-06 19:21:19 -0300

    feat: retorna lista ao encontrar palavra

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,26 @@
+from mimetypes import suffix_map
+
+
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    word_list = []
+    word_dict = {}
+    occurrences = []
+    count = 0
+    word_dict["palavra"] = word
 
+    for position in range(len(instance)):
+        path_name = instance.search(position)
+        word_dict["arquivo"] = path_name["nome_do_arquivo"]
 
-def search_by_word(word, instance):
+        for position, row in enumerate(path_name["linhas_do_arquivo"]):
+            if word in row.lower():
+                occurrences.append({"linha": position + 1})
+                count += 1
+        
+    word_dict["ocorrencias"] = occurrences
+    word_list.append(word_dict)
+
+    return word_list
+
+def search_by_word(path_name, instance):
     """Aqui irá sua implementação"""
