631b4f87bcb8c3e36de77e8fffedfd079c4930b9
Eder Santos <3derms@gmail.com>
2022-04-06 09:45:17 -0300

    retorna a menssagem via stdout da funçao process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,18 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    data_list = txt_importer(path_file)
+    number_of_rows = len(data_list)
+
+    dict_process = {}
+
+    dict_process["nome_do_arquivo"] = path_file
+    dict_process["qtd_linhas"] = number_of_rows
+    dict_process["linhas_do_arquivo"] = data_list
+
+    print(dict_process, file=sys.stdout)
 
 
 def remove(instance):
