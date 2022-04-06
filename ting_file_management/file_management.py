c905f23ddf1d2d7483993b2d79f7cdc7019a06bc
Eder Santos <3derms@gmail.com>
2022-04-05 21:08:23 -0300

    feat: ao importar com extencao invalida é exibida mensagem

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,8 +1,15 @@
+import sys
+
 def txt_importer(path_file):
     data_list = []
+
+    if path_file[len(path_file)-4:] != ".txt":
+        print("Formato inválido", file=sys.stderr)
+
     with open(path_file) as file:
         for row in file:
             new_row = row[:-1]
             if len(row) > 2:
                 data_list.append(new_row)
+
     return data_list
