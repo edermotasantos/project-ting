270b2ec71c43e58d0cc083b149ee2500314f3320
Eder Santos <3derms@gmail.com>
2022-04-06 21:34:41 -0300

    refactor: diminui complexidade

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,4 +1,3 @@
-from mimetypes import suffix_map
 
 
 def exists_word(word, instance):
@@ -18,31 +17,34 @@ def exists_word(word, instance):
                 count += 1
     if count == 0:
         return []
-        
+
     word_dict["ocorrencias"] = occurrences
     word_list.append(word_dict)
 
     return word_list
 
+
+def ocorr(path_n, path_name, occurrences):
+    for position, row in enumerate(path_n["linhas_do_arquivo"]):
+        if path_name in row.lower():
+            if row[-1:] != ".":
+                row = row + "."
+            occurrences.append({"linha": position + 1, "conteudo": row})
+    return occurrences
+
+
 def search_by_word(path_name, instance):
     word_list = []
     word_dict = {}
     occurrences = []
-    count = 0
     word_dict["palavra"] = path_name
 
     for position in range(len(instance)):
         path_n = instance.search(position)
         word_dict["arquivo"] = path_n["nome_do_arquivo"]
+        occurrences = ocorr(path_n, path_name, occurrences)
 
-        for position, row in enumerate(path_n["linhas_do_arquivo"]):
-            if path_name in row.lower():
-                if row[-1:] != ".":
-                    row = row + "."
-                occurrences.append({"linha": position + 1, "conteudo": row})
-                count += 1
-
-    if count == 0:
+    if occurrences == []:
         return []
 
     word_dict["ocorrencias"] = occurrences
