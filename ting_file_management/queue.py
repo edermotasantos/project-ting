3782f3026a0cfa6dc2bd21711ca0f87ab869666d
Eder Santos <3derms@gmail.com>
2022-04-14 19:24:00 -0300

    fix: corrige espaçamento em queue.py

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,20 +1,15 @@
 class Queue:
     FIRST_ELEMENT = 0
-
     def __init__(self):
         self._data = []
-
     def __len__(self):
         return len(self._data)
-
     def enqueue(self, value):
         self._data.append(value)
-
     def dequeue(self):
         if self._data:
             return self._data.pop(self.FIRST_ELEMENT)
         return None
-
     def search(self, index):
         if len(self._data) == 0 or index < 0:
             raise IndexError("list index out of range")
