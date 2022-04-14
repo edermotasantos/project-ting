06a7b09de41d4a5b53d0969c838830bb82d9aa45
Eder Santos <3derms@gmail.com>
2022-04-14 19:17:04 -0300

    fix: corrige espaçamento em file_process.py

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -7,20 +7,16 @@ def process(path_file, instance):
     dict_process = {}
     data_list = txt_importer(path_file)
     number_of_rows = len(data_list)
-
     if len(instance) != 0:
         for path_name in path_file_list:
             if path_file == path_name:
                 return len(instance)
-
     dict_process["nome_do_arquivo"] = path_file
     dict_process["qtd_linhas"] = number_of_rows
     dict_process["linhas_do_arquivo"] = data_list
-
     if len(instance) == 0:
         path_file_list.append(path_file)
         instance.enqueue(dict_process)
-
     print(dict_process, file=sys.stdout)
 
 
