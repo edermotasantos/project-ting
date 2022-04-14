76c3ba702f23a34f71edd4b4605b1b318f73a222
Eder Santos <3derms@gmail.com>
2022-04-14 19:22:34 -0300

    fix: corrige espaçamento em file_management.py

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -3,10 +3,8 @@ import sys
 
 def txt_importer(path_file):
     data_list = []
-
     if path_file[len(path_file)-4:] != ".txt":
         print("Formato inválido", file=sys.stderr)
-
     if path_file[
         len(path_file)-4:
     ] == ".txt" and path_file == "statics/arquivo_nao_existe.txt":
@@ -17,5 +15,4 @@ def txt_importer(path_file):
                 new_row = row[:-len("\n")]
                 if len(row) > 2:
                     data_list.append(new_row)
-
     return data_list
