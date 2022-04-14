d9e600e332b8c17ac9bd041e2b7f2f8274da9d0e
Eder Santos <3derms@gmail.com>
2022-04-14 19:14:57 -0300

    fix: corrige espaçamento em word_search.py

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -5,20 +5,16 @@ def exists_word(word, instance):
     word_dict = {}
     occurrences = []
     word_dict["palavra"] = word
-
     for position in range(len(instance)):
         path_name = instance.search(position)
         word_dict["arquivo"] = path_name["nome_do_arquivo"]
-
         for position, row in enumerate(path_name["linhas_do_arquivo"]):
             if word in row.lower():
                 occurrences.append({"linha": position + 1})
     if occurrences == []:
         return []
-
     word_dict["ocorrencias"] = occurrences
     word_list.append(word_dict)
-
     return word_list
 
 
@@ -36,16 +32,12 @@ def search_by_word(path_name, instance):
     word_dict = {}
     occurrences = []
     word_dict["palavra"] = path_name
-
     for position in range(len(instance)):
         path_n = instance.search(position)
         word_dict["arquivo"] = path_n["nome_do_arquivo"]
         occurrences = ocorr(path_n, path_name, occurrences)
-
     if occurrences == []:
         return []
-
     word_dict["ocorrencias"] = occurrences
     word_list.append(word_dict)
-
     return word_list
